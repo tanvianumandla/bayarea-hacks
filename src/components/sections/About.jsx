d2f8b83e624963369ed49ac5fd6dee3348e2f808
Tanvi Reddy Anumandla <tanvi@homes-air.lan>
2024-07-17 00:25:07 -0700

    started working on color scheme

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -6,9 +6,9 @@ const prizeAmt = 1600;
 
 const About = () => {
   return (
-    <div className="flex flex-col items-center bg-gradient-to-b from-light to-light pt-20 pb-40 pl-5 pr-5">
+    <div className="flex flex-col items-center bg-gradient-to-b from-primary to-primary pt-20 pb-40 pl-5 pr-5">
       <div className="pb-20">
-        <h1 className="text-6xl md:text-7xl font-bold transition-transform">About</h1>
+      <h1 className="text-6xl md:text-7xl font-bold transition-transform text-secondary">About</h1>
       </div>
       <div className="flex gap-5 max-w-7xl w-full flex-col md:flex-row">
         <div className="flex flex-col p-10 bg-gray-950/50 rounded-md w-full transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-indigo-800 hover:shadow-indigo-500">
@@ -24,7 +24,7 @@ const About = () => {
             covered food for everyone!
           </h3>
         </div>
-        <div className="flex flex-col p-10 primary rounded-md w-full transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-indigo-800 hover:shadow-indigo-500">
+        <div className="flex flex-col p-10 bg-gray-950/50 rounded-md w-full transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-indigo-800 hover:shadow-indigo-500">
           <h2 className="font-bold text-3xl inline-flex gap-5 py-3"><Trophy size={35} />Prizes</h2>
           <h3 className="font-normal text-lg">{prizes} prizes, including overall and track-specific awards, worth over ${prizeAmt} in value!</h3>
         </div>
--- src/components/sections/Schedule.jsx
+++ src/components/sections/Schedule.jsx
@@ -1,69 +1,70 @@
 const Schedule = () => {
   return (
-    <div className="flex flex-col items-center pt-20 pb-40 pl-5 pr-5 justify-center gap-20 bg-gradient-to-t from-gray-950 bg-gray-900 20">
+    <div className="flex flex-col items-center pt-20 pb-40 pl-5 pr-5 justify-center gap-20 bg-gradient-to-t from-primary primary">
       <div className="rounded-lg flex-col max-w-3xl w-full gap-5 flex">
-        <h1 className="font-bold text-6xl md:text-7xl text-center pb-10">
+        <h1 className="font-bold text-6xl md:text-7xl text-center pb-10 text-secondary">
           Schedule
         </h1>
-        <div className="flex flex-col px-10 py-5 bg-gradient-to-tr from-gray-900 to-[#25255a] rounded-3xl h-300 w-full shadow-2xl">
+
+        <div className="flex flex-col px-10 py-5 bg-gradient-to-tr from-light to-light rounded-3xl h-300 w-full shadow-2xl">
           <div className="flex py-3 gap-3 justify-between">
-            <h2 className=" text-lg text-nowrap">07:30 AM</h2>
-            <h2 className=" text-lg text-right">Check-in</h2>
+            <h2 className="text-lg text-nowrap text-black">07:30 AM</h2>
+            <h2 className="text-lg text-right text-black">Check-in</h2>
           </div>
           <div className="flex py-3 gap-3 justify-between">
-            <h2 className=" text-lg text-nowrap">08:30 AM</h2>
-            <h2 className=" text-lg text-right">Opening Ceremony</h2>
+            <h2 className="text-lg text-nowrap text-black">08:30 AM</h2>
+            <h2 className="text-lg text-right text-black">Opening Ceremony</h2>
           </div>
           <div className="flex py-3 gap-3 justify-between">
-            <h2 className=" text-lg text-nowrap">09:00 AM</h2>
-            <h2 className=" text-lg text-right">Hacking Begins</h2>
+            <h2 className="text-lg text-nowrap text-black">09:00 AM</h2>
+            <h2 className="text-lg text-right text-black">Hacking Begins</h2>
           </div>
           <div className="flex py-3 gap-3 justify-between">
-            <h2 className=" text-lg text-nowrap">09:30 AM</h2>
-            <h2 className=" text-lg text-right">Beginner Frontend Workshop</h2>
+            <h2 className="text-lg text-nowrap text-black">09:30 AM</h2>
+            <h2 className="text-lg text-right text-black">Beginner Frontend Workshop</h2>
           </div>
           <div className="flex py-3 gap-3 justify-between">
-            <h2 className=" text-lg text-nowrap">10:15 AM</h2>
-            <h2 className=" text-lg text-right">Intermediate React Workshop</h2>
+            <h2 className="text-lg text-nowrap text-black">10:15 AM</h2>
+            <h2 className="text-lg text-right text-black">Intermediate React Workshop</h2>
           </div>
           <div className="flex py-3 gap-3 justify-between">
-            <h2 className=" text-lg text-nowrap">10:30 AM</h2>
-            <h2 className=" text-lg text-right">Expertise AI/ML Workshop</h2>
+            <h2 className="text-lg text-nowrap text-black">10:30 AM</h2>
+            <h2 className="text-lg text-right text-black">Expertise AI/ML Workshop</h2>
           </div>
           <div className="flex py-3 gap-3 justify-between">
-            <h2 className=" text-lg text-nowrap">12:30 PM</h2>
-            <h2 className=" text-lg text-right">Lunch</h2>
+            <h2 className="text-lg text-nowrap text-black">12:30 PM</h2>
+            <h2 className="text-lg text-right text-black">Lunch</h2>
           </div>
           <div className="flex py-3 gap-3 justify-between">
-            <h2 className=" text-lg text-nowrap">03:00 PM</h2>
-            <h2 className=" text-lg text-right">Keynote Speaker #1</h2>
+            <h2 className="text-lg text-nowrap text-black">03:00 PM</h2>
+            <h2 className="text-lg text-right text-black">Keynote Speaker #1</h2>
           </div>
           <div className="flex py-3 gap-3 justify-between">
-            <h2 className=" text-lg text-nowrap">04:30 PM</h2>
-            <h2 className=" text-lg text-right">Keynote Speaker #2</h2>
+            <h2 className="text-lg text-nowrap text-black">04:30 PM</h2>
+            <h2 className="text-lg text-right text-black">Keynote Speaker #2</h2>
           </div>
           <div className="flex py-3 gap-3 justify-between">
-            <h2 className=" text-lg text-nowrap">06:30 PM</h2>
-            <h2 className=" text-lg text-right">Dinner</h2>
+            <h2 className="text-lg text-nowrap text-black">06:30 PM</h2>
+            <h2 className="text-lg text-right text-black">Dinner</h2>
           </div>
           <div className="flex py-3 gap-3 justify-between">
-            <h2 className=" text-lg text-nowrap">07:30 PM</h2>
-            <h2 className=" text-lg text-right">Coding Ends</h2>
+            <h2 className="text-lg text-nowrap text-black">07:30 PM</h2>
+            <h2 className="text-lg text-right text-black">Coding Ends</h2>
           </div>
           <div className="flex py-3 gap-3 justify-between">
-            <h2 className=" text-lg text-nowrap">08:00 PM</h2>
-            <h2 className=" text-lg text-right">Judging Begins</h2>
+            <h2 className="text-lg text-nowrap text-black">08:00 PM</h2>
+            <h2 className="text-lg text-right text-black">Judging Begins</h2>
           </div>
           <div className="flex py-3 gap-3 justify-between">
-            <h2 className=" text-lg text-nowrap">09:00 PM</h2>
-            <h2 className=" text-lg text-right">Awards and Closing Ceremony</h2>
+            <h2 className="text-lg text-nowrap text-black">09:00 PM</h2>
+            <h2 className="text-lg text-right text-black">Awards and Closing Ceremony</h2>
           </div>
         </div>
       </div>
       <h2 className="max-w-xl text-center text-lg">
-        <a className="font-normal">This schedule is </a>
-        <a className="font-extrabold">subject to change.</a>
-        <a className="font-normal">
+        <a className="font-normal text-black">This schedule is </a>
+        <a className="font-extrabold text-black">subject to change.</a>
+        <a className="font-normal text-black">
           {" "}
           Please check back periodically or right before the day of the
           hackathon.
