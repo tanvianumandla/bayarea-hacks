a59a75862118f90ec5da4a66f29b5464bca1074e
devmam999 <devmam999@gmail.com>
2024-09-04 17:16:40 -0700

    moved pranay down

--- src/components/sections/Team.jsx
+++ src/components/sections/Team.jsx
@@ -222,39 +222,6 @@ const Team = () => {
           </CardBody>
         </CardContainer>
 
-        <CardContainer className="w-full lg:w-56 lg:h-68">
-          <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-full flex flex-col items-center">
-            <CardItem translateZ="50" className="text-md font-bold text-black text-center">
-              Pranay Jain
-            </CardItem>
-            <CardItem as="p" translateZ="60" className="text-black text-sm max-w-sm mt dark:text-black">
-              Finance Lead
-            </CardItem>
-            <div className="mt-4 flex flex-col items-center">
-              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black">
-                <img
-                  src={pranayPng}
-                  width="125"
-                  height="125"
-                  className="rounded-full"
-                  alt="Pranay Jain"
-                />
-              </CardItem>
-              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black flex justify-center">
-                <a href="https://www.linkedin.com/in/pranay-jain-b9b287223/" target="_blank">
-                  <img
-                    src={linkedInPng}
-                    className="rounded-sm"
-                    width="25"
-                    height="25"
-                    alt="LinkedIn Logo"
-                  />
-                </a>
-              </CardItem>
-            </div>
-          </CardBody>
-        </CardContainer>
-
         <CardContainer className="w-full lg:w-56">
           <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-min flex flex-col items-center">
             <CardItem translateZ="50" className="text-md font-bold text-black text-center">
@@ -308,6 +275,40 @@ const Team = () => {
               </CardItem>
               <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black flex justify-center pt-4">
                 <a href="https://www.linkedin.com/in/poorvi-vijayananda-5a764a213/" target="_blank">
+                  <img
+                    src={linkedInPng}
+                    className="rounded-sm"
+                    width="25"
+                    height="25"
+                    alt="LinkedIn Logo"
+                  />
+                </a>
+              </CardItem>
+            </div>
+          </CardBody>
+        </CardContainer>
+      </div>
+      <div className="flex flex-wrap justify-center gap-4 max-w-5xl w-full mb-10">
+      <CardContainer className="w-full lg:w-56 lg:h-68">
+          <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-full flex flex-col items-center">
+            <CardItem translateZ="50" className="text-md font-bold text-black text-center">
+              Pranay Jain
+            </CardItem>
+            <CardItem as="p" translateZ="60" className="text-black text-sm max-w-sm mt dark:text-black">
+              Auxiliary
+            </CardItem>
+            <div className="mt-4 flex flex-col items-center">
+              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black">
+                <img
+                  src={pranayPng}
+                  width="125"
+                  height="125"
+                  className="rounded-full"
+                  alt="Pranay Jain"
+                />
+              </CardItem>
+              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black flex justify-center">
+                <a href="https://www.linkedin.com/in/pranay-jain-b9b287223/" target="_blank">
                   <img
                     src={linkedInPng}
                     className="rounded-sm"
