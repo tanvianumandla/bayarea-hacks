402ff7aa6fe9797629eeb8ad99179c97dd850d1d
Tanvi Reddy Anumandla <tanvi@Homes-MacBook-Air.local>
2024-09-07 13:38:27 -0700

    tracks

--- src/components/sections/Tracks.jsx
+++ src/components/sections/Tracks.jsx
@@ -10,19 +10,19 @@ const Tracks = () => {
         <div className="flex flex-col gap-10 p-10 bg-red rounded-lg transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black">
           <h2 className="font-bold text-3xl sm:text-4x inline-flex gap-5 text-black">
             <Megaphone size={35} />
-            Cybersecurity
+            Track
           </h2>
           <h3 className="font-normal text-lg text-black">
-            To be announced...
+            Track
           </h3>
         </div>
         <div className="flex flex-col gap-10 p-10 bg-red rounded-lg transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black">
           <h2 className="font-bold text-3xl sm:text-4xl inline-flex gap-5 text-black">
             <Megaphone size={35}/>
-            Health Care
+            Track
           </h2>
           <h3 className="font-normal text-lg text-black">
-            To be announced....
+            Cybersecurity
           </h3>
         </div>
         <div className="flex flex-col gap-10 p-10 bg-red rounded-lg transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black">
@@ -31,7 +31,7 @@ const Tracks = () => {
             Education
           </h2>
           <h3 className="font-normal text-lg text-black">
-            To be announced...
+            Healthcare
           </h3>
         </div>
       </div>
