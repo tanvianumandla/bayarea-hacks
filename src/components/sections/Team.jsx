aca5add593554b35e4a7c3f6a23ac5e7d393d971
devmam999 <107886293+devmam999@users.noreply.github.com>
2024-09-17 16:14:00 -0700

    Update Team.jsx

--- src/components/sections/Team.jsx
+++ src/components/sections/Team.jsx
@@ -2,6 +2,7 @@ import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
 import nishanthPng from "../../assets/nishanth.png";
 import ritvikPng from "../../assets/ritvik.png"
 import pranayPng from "../../assets/pranay.png";
+import deveshPng from "../../assets/devesh.png";
 import aahanPng from "../../assets/aahan.png";
 import JustinPng from "../../assets/Justin.png";
 import tanviPng from "../../assets/tanvi.png";
@@ -322,7 +323,38 @@ const Team = () => {
         </CardContainer>
 
         
-
+<CardContainer className="w-full lg:w-56 lg:h-72">
+          <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-full flex flex-col items-center">
+            <CardItem translateZ="50" className="text-md font-bold text-black text-center">
+              Devesh Mamidi
+            </CardItem>
+            <CardItem as="p" translateZ="60" className="text-black text-sm max-w-sm mt dark:text-black">
+              Tech
+            </CardItem>
+            <div className="mt-4 flex flex-col items-center">
+              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black">
+                <img
+                  src={deveshPng}
+                  width="125"
+                  height="125"
+                  className="rounded-full"
+                  alt="Toshar Singh"
+                />
+              </CardItem>
+              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black flex justify-center">
+                <a href="https://www.linkedin.com/in/devesh-mamidi-678998231/" target="_blank">
+                  <img
+                    src={linkedInPng}
+                    className="rounded-sm"
+                    width="25"
+                    height="25"
+                    alt="LinkedIn Logo"
+                  />
+                </a>
+              </CardItem>
+            </div>
+          </CardBody>
+        </CardContainer>
         <CardContainer className="w-full lg:w-56 lg:h-72">
           <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-full flex flex-col items-center">
             <CardItem translateZ="50" className="text-md font-bold text-black text-center">
