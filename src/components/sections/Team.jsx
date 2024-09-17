37cc3cfda13876b12b297cc352948a23104d341a
Tanvi Reddy Anumandla <tanvi@homes-air.lan>
2024-09-16 22:47:12 -0700

    changes

--- src/components/sections/Team.jsx
+++ src/components/sections/Team.jsx
@@ -6,10 +6,10 @@ import aahanPng from "../../assets/aahan.png";
 import JustinPng from "../../assets/Justin.png";
 import tanviPng from "../../assets/tanvi.png";
 import linkedInPng from "../../assets/linkedin.png";
-import deveshPng from "../../assets/devesh.png";
 import poorviPng from "../../assets/poorvi.png";
 import aaravPng from "../../assets/aarav.png";
 import aarnaPng from "../../assets/aarna.png"; 
+import tosharPng from "/Users/tanvi/Downloads/work/src/assets/toshar.png"
 
 const Team = () => {
   return (
@@ -21,7 +21,7 @@ const Team = () => {
       {/* First Row */}
       <div className="flex flex-wrap justify-center gap-4 max-w-5xl w-full mb-10">
         <CardContainer className="w-full lg:w-64">
-          <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-min flex flex-col items-center">
+          <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-full flex flex-col items-center">
             <CardItem translateZ="50" className="text-md font-bold text-black text-center">
               Aahan Shah
             </CardItem>
@@ -54,7 +54,7 @@ const Team = () => {
         </CardContainer>
 
         <CardContainer className="w-full lg:w-64">
-          <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-min flex flex-col items-center">
+          <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-full flex flex-col items-center">
             <CardItem translateZ="50" className="text-md font-bold text-black text-center">
               Ritvik Rustagi
             </CardItem>
@@ -89,7 +89,7 @@ const Team = () => {
 
       {/* Second Row */}
       <div className="flex flex-wrap justify-center gap-4 max-w-5xl w-full mb-10">
-        <CardContainer className="w-full lg:w-56 lg:h-68">
+        <CardContainer className="w-full lg:w-56 lg:h-72">
           <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-full flex flex-col items-center">
             <CardItem translateZ="50" className="text-md font-bold text-black text-center">
               Nishanth Koushik
@@ -154,7 +154,7 @@ const Team = () => {
           </CardBody>
         </CardContainer>
 
-        <CardContainer className="w-full lg:w-56 lg:h-68">
+        <CardContainer className="w-full lg:w-56 lg:h-72">
           <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-full flex flex-col items-center">
             <CardItem translateZ="50" className="text-md font-bold text-black text-center">
               Tanvi Anumandla
@@ -167,13 +167,13 @@ const Team = () => {
                 <img
                   src={tanviPng}
                   width="125"
-                  className="rounded-full"
                   height="125"
+                  className="rounded-full"
                   alt="Tanvi Anumandla"
                 />
               </CardItem>
               <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black flex justify-center">
-                <a href="https://www.linkedin.com/in/tanvianumandla/" target="_blank">
+                <a href="https://www.linkedin.com/in/tanvi-anumandla" target="_blank">
                   <img
                     src={linkedInPng}
                     className="rounded-sm"
@@ -190,41 +190,8 @@ const Team = () => {
 
       {/* Third Row */}
       <div className="flex flex-wrap justify-center gap-4 max-w-5xl w-full mb-10">
-        <CardContainer className="w-full lg:w-56 lg:h-72">
+        <CardContainer className="w-full lg:w-56 lg:h72">
           <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-full flex flex-col items-center">
-            <CardItem translateZ="50" className="text-md font-bold text-black text-center">
-              Devesh Mamidi
-            </CardItem>
-            <CardItem as="p" translateZ="60" className="text-black text-sm max-w-sm mt dark:text-black">
-              Tech
-            </CardItem>
-            <div className="mt-4 flex flex-col items-center">
-              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black">
-                <img
-                  src={deveshPng}
-                  width="125"
-                  height="125"
-                  className="rounded-full"
-                  alt="Devesh Mamidi"
-                />
-              </CardItem>
-              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black flex justify-center">
-                <a href="https://www.linkedin.com/in/devesh-mamidi/" target="_blank">
-                  <img
-                    src={linkedInPng}
-                    className="rounded-sm"
-                    width="25"
-                    height="25"
-                    alt="LinkedIn Logo"
-                  />
-                </a>
-              </CardItem>
-            </div>
-          </CardBody>
-        </CardContainer>
-
-        <CardContainer className="w-full lg:w-56">
-          <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-min flex flex-col items-center">
             <CardItem translateZ="50" className="text-md font-bold text-black text-center">
               Aarna Sahu
             </CardItem>
@@ -241,8 +208,8 @@ const Team = () => {
                   alt="Aarna Sahu"
                 />
               </CardItem>
-              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black flex justify-center ">
-                <a href="https://www.linkedin.com/in/aarna-sahu-2579b2234/" target="_blank">
+              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black flex justify-center">
+                <a href="https://www.linkedin.com/in/aarna-sahu" target="_blank">
                   <img
                     src={linkedInPng}
                     className="rounded-sm"
@@ -274,8 +241,8 @@ const Team = () => {
                   alt="Poorvi Vijayananda"
                 />
               </CardItem>
-              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black flex justify-center pt-4">
-                <a href="https://www.linkedin.com/in/poorvi-vijayananda-5a764a213/" target="_blank">
+              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black flex justify-center">
+                <a href="https://www.linkedin.com/in/poorvi-vijayananda" target="_blank">
                   <img
                     src={linkedInPng}
                     className="rounded-sm"
@@ -288,9 +255,8 @@ const Team = () => {
             </div>
           </CardBody>
         </CardContainer>
-      </div>
-      <div className="flex flex-wrap justify-center gap-4 max-w-5xl w-full mb-10">
-      <CardContainer className="w-full lg:w-56 lg:h-68">
+
+        <CardContainer className="w-full lg:w-56 lg:h-72">
           <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-full flex flex-col items-center">
             <CardItem translateZ="50" className="text-md font-bold text-black text-center">
               Pranay Jain
@@ -309,7 +275,7 @@ const Team = () => {
                 />
               </CardItem>
               <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black flex justify-center">
-                <a href="https://www.linkedin.com/in/pranay-jain-b9b287223/" target="_blank">
+                <a href="https://www.linkedin.com/in/pranay-jain/" target="_blank">
                   <img
                     src={linkedInPng}
                     className="rounded-sm"
@@ -322,6 +288,41 @@ const Team = () => {
             </div>
           </CardBody>
         </CardContainer>
+        <CardContainer className="w-full lg:w-56 lg:h-72">
+          <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-full flex flex-col items-center">
+            <CardItem translateZ="50" className="text-md font-bold text-black text-center">
+              Toshar Singh
+            </CardItem>
+            <CardItem as="p" translateZ="60" className="text-black text-sm max-w-sm mt dark:text-black">
+              Volunteer & Photographer
+            </CardItem>
+            <div className="mt-4 flex flex-col items-center">
+              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black">
+                <img
+                  src={tosharPng}
+                  width="125"
+                  height="125"
+                  className="rounded-full"
+                  alt="Toshar Singh"
+                />
+              </CardItem>
+              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black flex justify-center">
+                <a href="" target="_blank">
+                  <img
+                    src={linkedInPng}
+                    className="rounded-sm"
+                    width="25"
+                    height="25"
+                    alt="LinkedIn Logo"
+                  />
+                </a>
+              </CardItem>
+            </div>
+          </CardBody>
+        </CardContainer>
+
+        
+
         <CardContainer className="w-full lg:w-56 lg:h-72">
           <CardBody className="bg-gray-950/50 relative group/card transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-black hover:shadow-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border w-full h-full flex flex-col items-center">
             <CardItem translateZ="50" className="text-md font-bold text-black text-center">
@@ -340,6 +341,17 @@ const Team = () => {
                   alt="Aarav Garg"
                 />
               </CardItem>
+              <CardItem translateZ={20} className="py-2 rounded-xl text-xs font-normal dark:text-black flex justify-center">
+                <a href="https://www.linkedin.com/in/aarav-garg" target="_blank">
+                  <img
+                    src={linkedInPng}
+                    className="rounded-sm"
+                    width="25"
+                    height="25"
+                    alt="LinkedIn Logo"
+                  />
+                </a>
+              </CardItem>
             </div>
           </CardBody>
         </CardContainer>
@@ -349,3 +361,4 @@ const Team = () => {
 };
 
 export default Team;
+
